b3e9cf1d91d81cb5e83672cdb48ed24224955582
Andrew Lo <andrewlo@shaggy.cs.ucsb.edu>
2013-06-07 02:58:50 -0700

    Sorry, half-asleep during last commit. This is a real setup file.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
+CAN_INSTALL="y" # TRUE if not found
+
 # Check for existence of .vimrc
 if [ -f ~/.vimrc ]; then
     # Overwrite?
-    read -p "-- ~/.vimrc already exists. Overwrite? [y/N] "
-    if [ "$REPLY" = "y" ]; then
-        # Copy .vimrc over
-        echo " -> Copying .vimrc to ~/.vimrc..."
-        cp .vimrc ~/.vimrc
-    fi
-
-    # Add extra setup things later if needd
-
-    # Done
-    echo "-- Done!"
+    read -p "-- ~/.vimrc already exists. Overwrite? [y/N] " CAN_INSTALL
 fi
+
+# Copy over our version
+if [ "$CAN_INSTALL" = "y" ]; then
+    # Copy .vimrc over
+    echo " -> Copying .vimrc to ~/.vimrc..."
+    cp .vimrc ~/.vimrc
+else
+    echo "-- Aborting!"
+    exit
+fi
+
+# Install Vundle
+# ---------------
+# Adapted from:
+# http://www.erikzaadi.com/2012/03/19/auto-installing-vundle-from-your-vimrc/)
+if [ ! -f ~/.vim/bundle/vundle/README.md ]; then
+    echo "-- Installing Vundle..."
+
+    echo " -> Make Vundle directory"
+    mkdir -p ~/.vim/bundle > /dev/null 2>&1
+
+    echo " -> Clone GitHub repository"
+    git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle > /dev/null
+fi
+
+# Auto install bundles
+echo "-- Installing bundles..."
+vim +BundleInstall +qall
+
+# Add extra setup things later if needd
+
+# Done
+echo "-- Done!"
 
