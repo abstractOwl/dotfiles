7bcb01f2691ab7d72732807e412ee8182242661a
AbstractOwl <abstractOwl@users.noreply.github.com>
2014-08-12 21:30:00 -0700

    Add python dev tools

--- provision.sh
+++ provision.sh
@@ -8,3 +8,7 @@ pkg_list=$(cat config/packages/*.list)
 sudo $APT_PREF -y update
 sudo $APT_PREF -y install $pkg_list
 
+# Set up Python dev
+sudo pip install virtualenv virtualenvwrapper
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
+echo 'export PROJECT_HOME="/workspace"' >> ~/.profile
