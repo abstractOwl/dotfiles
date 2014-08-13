0f803f548ddfa826f0f4ca864468ad965189047b
AbstractOwl <abstractOwl@users.noreply.github.com>
2014-08-12 21:54:45 -0700

    Add pylint as well

--- provision.sh
+++ provision.sh
@@ -9,6 +9,6 @@ sudo $APT_PREF -y update
 sudo $APT_PREF -y install $pkg_list
 
 # Set up Python dev
-sudo pip install virtualenv virtualenvwrapper
+sudo pip install pylint virtualenv virtualenvwrapper
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
 echo 'export PROJECT_HOME="/workspace"' >> ~/.profile
