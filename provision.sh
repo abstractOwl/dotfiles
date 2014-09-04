e3f755c495ecfb3afa0545115726268bede8321a
AbstractOwl <abstractOwl@users.noreply.github.com>
2014-09-04 06:49:55 -0700

    provision.sh: Added pythonpy

--- provision.sh
+++ provision.sh
@@ -9,6 +9,6 @@ sudo $APT_PREF -y update
 sudo $APT_PREF -y install $pkg_list
 
 # Set up Python dev
-sudo pip install pylint virtualenv virtualenvwrapper
+sudo pip install pylint virtualenv virtualenvwrapper pythonpy
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
 echo 'export PROJECT_HOME="/workspace"' >> ~/.profile
